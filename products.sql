0b09d7d8c44dbe486c15158d4f096ee9b327fa90
Eveloth <vadim.stepanov.web@gmail.com>
2024-05-03 01:57:54 +0300

    add query

---
+++ products.sql
@@ -0,0 +1,18 @@
+-- SETUP
+
+CREATE TABLE IF NOT EXISTS products (
+  id INT IDENTITY (1, 1) PRIMARY KEY,
+  name NVARCHAR(512) NOT NULL,
+  caterory_id INT,
+  CONSTRAINT fk_category FOREIGN KEY (category_id)
+  REFERENCES categories(id)
+)
+
+CREATE TABLE IF NOT EXISTS categories (
+  id INT IDENTITY (1, 1) PRIMARY KEY,
+  name NVARCHAR(512) NOT NULL,
+)
+
+-- QUERY
+
+SELECT p.name, c.name FROM products p LEFT JOIN categories c ON p.caterory_id = c.id
