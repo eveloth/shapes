b632e11bf4345c67063e8ed11387369cdc3e7389
Vadim Stepanov <vadim.stepanov.web@gmail.com>
2024-05-03 14:17:51 +0300

    add semicolons

--- products.sql
+++ products.sql
@@ -6,13 +6,13 @@ CREATE TABLE IF NOT EXISTS products (
   caterory_id INT,
   CONSTRAINT fk_category FOREIGN KEY (category_id)
   REFERENCES categories(id)
-)
+);
 
 CREATE TABLE IF NOT EXISTS categories (
   id INT IDENTITY (1, 1) PRIMARY KEY,
   name NVARCHAR(512) NOT NULL,
-)
+);
 
 -- QUERY
 
-SELECT p.name, c.name FROM products p LEFT JOIN categories c ON p.caterory_id = c.id
+SELECT p.name, c.name FROM products p LEFT JOIN categories c ON p.caterory_id = c.id;
